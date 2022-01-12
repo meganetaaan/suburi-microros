c4f2d5383fc88f79c927ce1690a65034b0ffe08b
Shinya Ishikawa <ishikawa@rt-net.jp>
2022-01-12 02:41:09 +0000

    Print usage

--- configure_and_build.sh
+++ configure_and_build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-ros2 run micro_ros_setup configure_firmware.sh complex_msg_publisher
+if [ $# -ne 1 ]; then
+  echo "usage: ./configure_firmware.sh [node_name]" 1>&2
+  exit 1
+fi
+
+ros2 run micro_ros_setup configure_firmware.sh $1
 ros2 run micro_ros_setup build_firmware.sh
 ros2 run micro_ros_setup flash_firmware.sh
